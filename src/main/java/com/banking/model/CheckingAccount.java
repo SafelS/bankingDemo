c4309253a3a45337668cce6d192c541c386915b5
SafelS <safel_safel@outlook.de>
2026-02-28 15:26:01 +0300

    Added CheckingAccount class

--- src/main/java/com/banking/model/CheckingAccount.java
+++ src/main/java/com/banking/model/CheckingAccount.java
@@ -1,4 +1,40 @@
 package com.banking.model;
 
-public class CheckingAccount {
+import com.banking.exception.InsufficientFundsException;
+
+public class CheckingAccount extends Account implements Transferable{
+
+    private double draftLimit;
+
+    public CheckingAccount(String id, String owner, double initialBalance, double draftLimit){
+        super(id, owner, initialBalance);
+
+        if(draftLimit < 0){
+            throw new IllegalArgumentException("Draft Limit cannot be negative");
+        }
+
+        this.draftLimit = draftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount){
+
+        if(amount > getBalance() + draftLimit){
+            throw new InsufficientFundsException("Overdraft limit exceeded");
+        }
+
+        super.withdraw(amount);
+
+    }
+
+    @Override
+    public void transfer(Account target, double amount) {
+        if(target == null){
+            throw new IllegalArgumentException("Target account cannot be null");
+        }
+
+        this.withdraw(amount);
+        target.deposit(amount);
+
+    }
 }
